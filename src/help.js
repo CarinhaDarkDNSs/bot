f7092591db36a84d939b4adfa612fbae1d77830e
CarinhaDarkDNSs <zenixmc.suporte@gmail.com>
2020-12-31 10:28:51 -0300

    First cmmit

---
+++ src/help.js
@@ -0,0 +1,18 @@
+const help = (prefix) => {
+	return `🔘 *BlazeHosting - BOT* 🔘\n\n
+📲 *Comandos:* 📲
+• command: *${prefix}marcar*
+desc : marque todos membros do grupo.\n\n
+• command:  *${prefix}whatsapp*
+desc: pegar o link do grupo.\n\n
+• command:  *${prefix}discord*
+desc: pegar o link do grupo.\n\n
+• command:  *${prefix}banir*
+desc: banir um membro do grupo.\n\n
+• command:  *${prefix}add*
+desc: adicionar um membro no grupo. *(somente admins)*\n\n\n`
+}
+
+exports.help = comandos
+
+
